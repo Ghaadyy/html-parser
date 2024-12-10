a34951e0997e02d2ee7ad40ccd0038f2de8f3834
Josef04 <josefsamara04@gmail.com>
2024-12-10 20:25:40 +0200

    feat: Define DOMTree structure and add tagName and addElement functions

--- DOM.hs
+++ DOM.hs
@@ -1,20 +1,22 @@
 module DOM where
 
--- import qualified Data.Map as Map
+import qualified Data.Map as Map
 
--- type HTMLAttributes = Map.Map String String
 type HTMLTag = String
 type HTMLContent = String
+type Attributes = Map.Map String String
 
-data DOMTree = EmptyTree 
-    | HTMLElement HTMLTag [DOMTree]  
+data DOMTree 
+    = EmptyTree 
+    | HTMLElement HTMLTag Attributes [DOMTree]  
     | TextNode HTMLContent
     deriving (Show, Eq)
 
 addElement :: DOMTree -> DOMTree -> DOMTree
-addElement tree el = EmptyTree
+addElement (HTMLElement tag attrs children) el = HTMLElement tag attrs (children ++ [el])
+addElement EmptyTree el = el
+addElement tree _ = tree
 
 tagName :: DOMTree -> HTMLTag
-tagName (HTMLElement tagName@(head:tail) _)  
-    | head == '/' = tail
-    | otherwise = tagName
+tagName (HTMLElement tagName _ _) = tagName
+tagName _ = ""
