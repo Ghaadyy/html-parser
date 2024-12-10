585aa37c98b3e2f98fefd8a1bb3c6db71ccdec47
Josef04 <josefsamara04@gmail.com>
2024-12-10 20:25:58 +0200

    refactor: Simplify combinators and remove redundant parsers
    - Removed textParser, whitespaceParser, and unnecessary lexer logic
    - Unified tag parsing logic into a single tagParser for both opening and closing tags
    - Retained only essential combinators for parser logic (satisfy, char, string, etc.)
    - Improved Alternative and Monad instance logic

--- Combinators.hs
+++ Combinators.hs
@@ -2,60 +2,51 @@
 
 module Combinators where
 
+import qualified Data.Map as Map
 import Control.Applicative (Alternative(..))
 import Data.Char (isSpace, isAsciiLower, isAsciiUpper)
 import GHC.Unicode (isDigit)
-import Debug.Trace (trace)
 import DOM (DOMTree(HTMLElement, EmptyTree, TextNode))
 
 newtype Parser a = Parser {
-    runParser :: String -> Either [Error] (a, String)
+  runParser :: String -> Either [Error] (a, String)
 }
 
 data Error
-    = EndOfInput
-    | Unexpected
-    | Empty
-    | TagsNotMatched
-    deriving (Eq, Show)
+  = EndOfInput
+  | Unexpected
+  | Empty
+  | TagsNotMatched
+  deriving (Eq, Show)
 
 data Token
-    = Tag String
-    | Text String
-    | Whitespace String
-    | Epsilon
-    deriving Show
+  = Tag String       
+  | ClosingTag String 
+  | Text String
+  | Whitespace String
+  | Epsilon
+  deriving (Eq, Show)
 
 instance Functor Parser where
-  fmap :: (a -> b) -> Parser a -> Parser b
   fmap f (Parser p) = Parser $ \input -> do
     (output, rest) <- p input
     pure (f output, rest)
 
 instance Applicative Parser where
-  pure :: a -> Parser a
   pure a = Parser $ \input -> Right (a, input)
-
-  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
   Parser f <*> Parser p = Parser $ \input -> do
     (f', rest) <- f input
     (output, rest') <- p rest
     pure (f' output, rest')
 
 instance Monad Parser where
-  return :: a -> Parser a
   return = pure
-
-  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
   Parser p >>= k = Parser $ \input -> do
     (output, rest) <- p input
     runParser (k output) rest
 
 instance Alternative Parser where
-  empty :: Parser a
   empty = Parser $ \_ -> Left [Empty]
-
-  (<|>) :: Parser a -> Parser a -> Parser a
   Parser l <|> Parser r = Parser $ \input ->
     case l input of
       Left err ->
@@ -66,10 +57,10 @@ instance Alternative Parser where
 
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy predicate = Parser $ \case
-    [] -> trace "End of input" $ Left [EndOfInput]
-    hd : rest
-      | predicate hd -> Right (hd, rest) -- trace ("Consumed: " ++ [hd]) $
-      | otherwise    -> Left [Unexpected] -- trace ("Unexpected: " ++ [hd]) $
+  [] -> Left [EndOfInput]
+  hd : rest
+    | predicate hd -> Right (hd, rest)
+    | otherwise    -> Left [Unexpected]
 
 char :: Char -> Parser Char
 char a = satisfy (== a)
@@ -90,39 +81,12 @@ many1 p = do
   xs <- many1 p <|> return []
   return (x:xs)
 
-whitespaceParser :: Parser Token
-whitespaceParser = do
-    ws <- many1 (satisfy isSpace)
-    return $ Whitespace ws
+tagParser :: Parser Token 
+tagParser = 
+  (Tag <$> (char '<' *> many1 (satisfy (\c -> isAsciiLower c || isAsciiUpper c || isDigit c || c == '-')) <* char '>')) 
+  <|> (ClosingTag <$> (string "</" *> many1 (satisfy (\c -> isAsciiLower c || isAsciiUpper c || isDigit c || c == '-')) <* char '>'))
 
-tagOpen :: Parser DOMTree
-tagOpen = do
-  _ <- char '<'
-  tagName <- many' (satisfy isAsciiLower)
-  _ <- char '>'
-  return $ HTMLElement tagName []
-
-tagClose :: Parser DOMTree
-tagClose = do
-  _ <- string "</"
-  tagName <- many' (satisfy isAsciiLower)
-  _ <- char '>'
-  return $ HTMLElement ("/" ++ tagName) []
-
-textParser :: Parser DOMTree
+textParser :: Parser DOMTree 
 textParser = do
   txt <- many1 (satisfy (\c -> isAsciiLower c || isAsciiUpper c || isDigit c || isSpace c))
   return $ TextNode txt
-
--- test input
-input :: String
-input = "<html><ol><li>test</li><li>another item</li></ol></html>"
-
--- tagParser :: Parser Token
--- tagParser = tagOpen <|> tagClose
-
--- lexer :: Parser [Token]
--- lexer = many' (whitespaceParser <|> textParser <|> tagParser)
-
--- runLexer :: String -> Either [Error] [Token]
--- runLexer input = fmap fst (runParser lexer input)
