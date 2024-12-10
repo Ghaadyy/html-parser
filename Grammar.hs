30fee454c74217e23587e30ef6bde2ee0e4ea14f
Josef04 <josefsamara04@gmail.com>
2024-12-10 20:26:04 +0200

    fix: Ensure TagsNotMatched is returned for unmatched HTML tags
    - Updated html parser to check for mismatched tags
    - Modified tagClose to return TagsNotMatched instead of Unexpected
    - Added textParser logic to correctly parse text nodes within the DOM

--- Grammar.hs
+++ Grammar.hs
@@ -2,22 +2,28 @@ module Grammar where
 
 import Combinators
 import Control.Applicative (Alternative(..))
-import Debug.Trace
+import qualified Data.Map as Map
 import DOM (DOMTree(HTMLElement), tagName)
-
--- Basic HTML Grammar
--- html     := tagOpen (text|html)? tagClose | epsilon
--- text     := ...
--- tagOpen  := ...
--- tagClose := ...
 
 html :: Parser DOMTree
 html = do
-    tagTree <- tagOpen
-    children <- many' (textParser <|> html)
-    tagCloseTree <- tagClose
-    if tagName tagTree /= tagName tagCloseTree
-        then Parser $ \input -> Left [TagsNotMatched]
-    else return (HTMLElement (tagName tagTree) children)
--- this does not *properly* take into account the matching of tags yet
--- we should handle error propagation and aggregation properly.
+  tagTree <- tagOpen
+  children <- many' (textParser <|> html)
+  tagCloseTree <- tagClose
+  if tagName tagTree /= tagName tagCloseTree
+    then Parser $ \input -> Left [TagsNotMatched]
+    else return (HTMLElement (tagName tagTree) Map.empty children)
+
+tagOpen :: Parser DOMTree
+tagOpen = do
+  token <- tagParser
+  case token of
+    Tag tagName -> return $ HTMLElement tagName Map.empty []
+    _ -> Parser $ \_ -> Left [TagsNotMatched]
+
+tagClose :: Parser DOMTree
+tagClose = do
+  token <- tagParser
+  case token of
+    ClosingTag tagName -> return $ HTMLElement tagName Map.empty []
+    _ -> Parser $ \_ -> Left [TagsNotMatched]
